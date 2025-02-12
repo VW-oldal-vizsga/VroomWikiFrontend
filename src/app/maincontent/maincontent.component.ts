5706b200e02c625a9045fc80aab3f5a4e3251cb4
zagyvatamas <xnoszix@gmail.com>
2025-02-12 11:11:35 +0100

    bootstrap accordion swap with custom accordion

--- src/app/maincontent/maincontent.component.ts
+++ src/app/maincontent/maincontent.component.ts
@@ -1,11 +1,25 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-maincontent',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './maincontent.component.html',
   styleUrl: './maincontent.component.css'
 })
 export class MaincontentComponent {
+  accordionData = [
+    { title: "Első", content: "Első tartalom", open: false},
+    { title: "Második", content: "Első tartalom", open: false},
+    { title: "Harmadik", content: "Első tartalom", open: false},
+    { title: "Negyedik", content: "Első tartalom", open: false}
 
+  ]
+
+  activeIndex: number | null = null;
+
+
+  toggle(index: number): void {
+    this.accordionData[index].open = !this.accordionData[index].open;
+  }
 }
