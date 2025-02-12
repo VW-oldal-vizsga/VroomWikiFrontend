7286919574d9cc1458487c71cf74ba002816a00a
zagyvatamas <xnoszix@gmail.com>
2025-02-12 12:36:12 +0100

    style structure changes

--- src/app/oldmodels/oldmodels.component.ts
+++ src/app/oldmodels/oldmodels.component.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../navbar/navbar.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-oldmodels',
   standalone:true,
-  imports: [NavbarComponent],
+  imports: [NavbarComponent, CommonModule],
   templateUrl: './oldmodels.component.html',
   styleUrl: './oldmodels.component.css'
 })
 export class OldmodelsComponent {
-  
+  cardData = [
+    { image:'assets/images/poloVivo.png', text: "Polo Vivo"},
+    { image:"assets/images/taigo.jpg", text: "Taigo"},
+    { image:"http://www.kepfeltoltes.eu/view.php?filename=4462018_Volkswagen_Touare.jpg", text: "Touareg"},
+    { image:"http://www.kepfeltoltes.eu/view.php?filename=3522018_Volkswagen_Arteon.jpg", text: "Arteon"}
+  ]
 }
