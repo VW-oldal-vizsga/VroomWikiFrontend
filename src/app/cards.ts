a58fadd82e1b00969b64f897e96aa7f78bbdffc3
zagyvatamas <xnoszix@gmail.com>
2025-02-26 08:54:37 +0100

    responsive changes

--- src/app/cards.ts
+++ src/app/cards.ts
@@ -1,5 +1,5 @@
 export const cardData = [
-    { image: 'assets/poloVivo.png', text: 'Polo Vivo', id: 'Polo-model', description: 'Egy VW model'  },
+    { image: 'assets/poloVivo.png', text: 'Polo Vivo', id: 'Polo-model', description: 'Egy VW model dadsadsad adsadasdasd adsadasdasdasdas dasdasd adsada adsadas dsad adssssssssss'  },
     { image: 'assets/taigo.jpg', text: 'Tesla', id: 'Tesla-model', description: 'Egy elektromos model'  },
     { image: 'assets/taigo.jpg', text: 'Taigo', id: 'Taigo-model', description: 'Egy másik VW model'  },
     { image: 'assets/taigo.jpg', text: 'Bmw', id: 'Bmw-model', description: 'Egy bmw model' },
