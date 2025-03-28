c3945a88081f336b7b1e46423bb0419e62a17857
zagyvatamas <xnoszix@gmail.com>
2025-03-28 10:07:34 +0100

    mainpage upgrade

--- src/models/mainPageData.interface.ts
+++ src/models/mainPageData.interface.ts
@@ -1,5 +1,9 @@
 export interface IMainData {
     description: string,
     owner:string,
+    employees:string,
+    factories:string,
+    chairman:string,
+    profit:string,
     imageBase64:string
 }
